e16f5fe7776b0e6a947ba2a77f26800fd53a3b9e
alf <alfredo.d@freshclinics.com>
2024-07-07 22:16:52 +0800

    chore: update loader size on mobile

--- src/app/_components/page-loader.tsx
+++ src/app/_components/page-loader.tsx
@@ -4,10 +4,10 @@ const PageLoader = () => {
   return (
     <div className="-m-2 flex h-screen items-center justify-center bg-gray-600/opacity-50">
       <div className="relative">
-        <div className="center-fixed h-64 w-64 animate-pulse rounded-full bg-stone-500">
+        <div className="center-fixed sm:h-64 h-32 sm:w-64 w-32 animate-pulse rounded-full bg-stone-500">
           {" "}
         </div>
-        <div className="center-fixed flex h-60 w-60 flex-col items-center justify-center gap-2 rounded-full bg-white p-10">
+        <div className="center-fixed flex sm:h-60 h-28 sm:w-60 w-28 flex-col items-center justify-center gap-2 rounded-full bg-white p-10">
           <ReloadIcon className="animate-spin" />
           Fetching data ...
         </div>
