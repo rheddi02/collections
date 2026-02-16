e132764b497260a943b6ab3ab64018cd560eaf6f
rheddi02 <rheddi02@gmail.com>
2026-02-16 16:42:31 +0800

    Chore: added dotenv pkg

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,15 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import { defineConfig, env } from "prisma/config";
+
+dotenv.config();
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrations: { 
+  migrations: {
     path: 'prisma/migrations',
     seed: 'tsx prisma/seed.ts',
   },
-  datasource: { 
-    url: env("DATABASE_URL") 
+  datasource: {
+    url: env("DATABASE_URL")
   }
 });
