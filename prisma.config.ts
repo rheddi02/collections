e9cb48a557e2449c86fac54d7ad852dd4317e021
rheddi02 <rheddi02@gmail.com>
2026-02-16 18:24:00 +0800

    chore: update to prisma 7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: { 
-    url: process.env.POSTGRES_PRISMA_URL
+    url: env("POSTGRES_PRISMA_URL")
   }
 });
