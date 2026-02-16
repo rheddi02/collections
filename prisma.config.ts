f3f458d1f0e48e91ed2d74fbafd7802fd30a6074
rheddi02 <rheddi02@gmail.com>
2026-02-16 17:00:30 +0800

    Chore: added dotenv pkg

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,7 @@
-import 'dotenv/config'
+import dotenv from "dotenv";
 import { defineConfig, env } from "prisma/config";
+
+dotenv.config();
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
